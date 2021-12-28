2b6fed090ed8b9389e96d5014d108d0b891b423d
yattinda <yattinda@gmail.com>
2021-12-28 23:32:50 +0900

    Fix blank

--- uber_pickups.py
+++ uber_pickups.py
@@ -26,7 +26,7 @@ data_load_state.text("Done! (using st.cache)")
 if st.checkbox('Show raw data'):
     st.subheader('Raw data')
     st.write(data)
-    
+
 st.subheader('Number of pickups by hour')
 hist_values = np.histogram(
     data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
