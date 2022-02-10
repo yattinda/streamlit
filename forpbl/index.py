fbf1057457621138e62b07b2aedcc17c2c81580d
yattinda <yattinda@gmail.com>
2022-02-10 15:39:33 +0900

    Fix for lint

--- forpbl/index.py
+++ forpbl/index.py
@@ -1,28 +1,28 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import csv
 import requests
 from bs4 import BeautifulSoup
-import time
-from tqdm import tqdm
 
 st.title('住みやすい街検索')
-st.write('### test')
+st.write('### 衛星データを活用して街の住みやすさをお届けします')
+
 
 @st.cache
 def lord(filename):
     content = np.loadtxt(filename, delimiter=',', skiprows=1,)
     return content
 
+
 @st.cache
 def neighbourhood(p0, ps):
     L = np.array([])
     for i in range(ps.shape[0]):
-        norm = np.sqrt( (ps[i][1] - p0[0])*(ps[i][1] - p0[0]) +
-                        (ps[i][2] - p0[1])*(ps[i][2] - p0[1]) )
+        norm = np.sqrt((ps[i][1] - p0[0])*(ps[i][1] - p0[0]) +
+                       (ps[i][2] - p0[1])*(ps[i][2] - p0[1]))
         L = np.append(L, norm)
     return ps[np.argmin(L)]
+
 
 @st.cache
 def coordinate(address):
@@ -42,6 +42,7 @@ def coordinate(address):
     longitude = soup.find('lng').string
     return [longitude, latitude]
 
+
 with st.sidebar:
     with st.form(key='input'):
         st.title('検索')
@@ -54,20 +55,15 @@ if run:
     longlat = (coordinate(address))
     longlat_float = [float(n) for n in longlat]
     content_ndvi = np.array(lord("data/NDVI_Japan.csv"))
-    content_air = np.array(lord("data/air_Japan.csv"))
-    nearrest_coor_ndvi  = neighbourhood(longlat_float, content_ndvi)
-    nearrest_coor_air  = neighbourhood(longlat_float, content_air)
+    content_air = np.array(lord("data/NO2_Japan.csv"))
+    nearrest_coor_ndvi = neighbourhood(longlat_float, content_ndvi)
+    nearrest_coor_air = neighbourhood(longlat_float, content_air)
     nearrest_longlat = {
-     'lon' : [longlat_float[0]],
-     'lat' : [longlat_float[1]]
+     'lon': [longlat_float[0]],
+     'lat': [longlat_float[1]]
     }
-    # st.write(longlat_float)
-    # st.write((nearrest_coor_ndvi[1], nearrest_coor_ndvi[2], nearrest_coor_ndvi[3]))
-    st.write("## 植生スコア : {}　空気スコア : {}".format(nearrest_coor_ndvi[3], nearrest_coor_air[2]))
-    st.bar_chart({
-    "スコア": [nearrest_coor_ndvi[3], nearrest_coor_air[2]],
-    # "空気スコア": [nearrest_coor_air[2]]
-    })
+    st.write("## 植生スコア : {}　NO2スコア : {}".format(nearrest_coor_ndvi[2], nearrest_coor_air[2]-1))
+    st.bar_chart({"スコア": [nearrest_coor_ndvi[2], nearrest_coor_air[2]]})
     map_data = pd.DataFrame(nearrest_longlat)
-    st.map(map_data, zoom = 13)
+    st.map(map_data, zoom=13)
     data_load_state.success("Success!!")
